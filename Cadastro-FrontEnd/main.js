fb32f03f9a2dfe4ae2848988201dc167f629b489
RaffaelSchemmer <elias.coelho92004@gmail.com>
2022-05-21 14:03:22 -0300

    Added button sound effects, new font-family and new input placeholder and typing area colors

--- Cadastro-FrontEnd/main.js
+++ Cadastro-FrontEnd/main.js
@@ -19,15 +19,25 @@
 //     fnMask(el)
 // })
 
-
 const actionBar = document.querySelector("div.action-bar")
 const btAdd = actionBar.querySelector(".bt-add")
+const btSearch = actionBar.querySelector(".bt-search")
 const container = document.querySelector(".container-data")
 const templateModalAluno = container.querySelector("template.aluno")
 
 btAdd.addEventListener("click", () => {
     const cloneModal = templateModalAluno.content.cloneNode(true)
     container.prepend(cloneModal)
+
+    const audio = new Audio('./click_sound.wav')
+    audio.play()
+    return
+})
+
+btSearch.addEventListener("click", () => {
+    const audio = new Audio('./click_sound.wav')
+    audio.play()
+    return
 })
 
 container.addEventListener("click", ev => {
@@ -35,5 +45,20 @@ container.addEventListener("click", ev => {
     if (btClose) {
         const modal = ev.target.closest(".modal")
         modal.remove()
+    }
+})
+
+container.addEventListener("click", ev => {
+    const btClose = ev.target.closest(".bt-close")
+    const btSave = ev.target.closest(".bt-save")
+    if (btClose) {
+        const audio = new Audio('./close_modal.wav')
+        audio.play()
+        return
+    }
+    if (btSave) {
+        const audio = new Audio('./save_sound.wav')
+        audio.play()
+        return
     }
 })
